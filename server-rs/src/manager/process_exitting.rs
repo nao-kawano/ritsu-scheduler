2ee21b1ad53b3d7dd1b97dc7d6f0356869708e31
Naoyuki Kawano <nao.kawano.dev@gmail.com>
2025-06-16 01:53:28 +0900

    server-rs: Implements process exitting

--- server-rs/src/manager/process_exitting.rs
+++ server-rs/src/manager/process_exitting.rs
@@ -6,54 +6,113 @@ extern crate log;
 #[allow(unused_imports)]
 use log::{debug, error, info, trace, warn};
 
-use dps_message::Message;
+use dps_message::{Message, MessageType};
 
 use super::EventResult;
+use super::ManagerState;
+use super::client_status::ClientState;
 use super::context::ManagerContext;
 use super::process::ManagerProc;
+
+#[cfg(test)]
+#[path = "process_exitting_test.rs"]
+mod process_exitting_test;
+
+/* -------------------------------------------------------------------------- */
 
 const LOG_TAG: &str = "StateExitting";
 
 pub struct ManagerProcExitting;
 impl ManagerProc for ManagerProcExitting {
-    fn enter_state(&self, context: &mut ManagerContext) {
-        // TODO: implements.
-        trace!("{}: enter_state\n", LOG_TAG);
+    fn enter_state(&self, _context: &mut ManagerContext) {
+        trace!("{}: enter_state", LOG_TAG);
     }
 
-    fn on_cycle_start(&self, context: &mut ManagerContext, _cycle: u64) -> EventResult {
-        // TODO: implements.
-        trace!("{}: on_cycle_start\n", LOG_TAG);
+    fn on_cycle_start(&self, _context: &mut ManagerContext, _cycle: u64) -> EventResult {
+        trace!("{}: on_cycle_start (nop)", LOG_TAG);
         Ok(vec![])
     }
 
-    fn on_client_join(&self, context: &mut ManagerContext, _message: &Message) -> EventResult {
-        // TODO: implements.
-        trace!("{}: on_client_join\n", LOG_TAG);
-        Ok(vec![])
+    fn on_client_join(&self, _context: &mut ManagerContext, message: &Message) -> EventResult {
+        trace!("{}: on_client_join id={}", LOG_TAG, message.client_id);
+        Err(format!("invalid Join from {}", message.client_id))
     }
 
-    fn on_client_ready(&self, context: &mut ManagerContext, _message: &Message) -> EventResult {
-        // TODO: implements.
-        trace!("{}: on_client_ready\n", LOG_TAG);
-        Ok(vec![])
+    fn on_client_ready(&self, context: &mut ManagerContext, message: &Message) -> EventResult {
+        trace!("{}: on_client_ready id={}", LOG_TAG, message.client_id);
+        let mut responses: Vec<Message> = Vec::new();
+        // update client state.
+        if let Some(client) = context.clients.get_mut(&message.client_id) {
+            if client.state == ClientState::Idle {
+                info!(
+                    "{}: send error to client {} ready",
+                    LOG_TAG, message.client_id
+                );
+                client.set_client_state(ClientState::Exitting);
+                responses.push(Message::new(MessageType::Error, message.client_id, None).unwrap());
+            } else {
+                warn!(
+                    "{}: client {} is not in Idle, dropped.",
+                    LOG_TAG, message.client_id
+                );
+            }
+        }
+        Ok(responses)
     }
 
-    fn on_client_done(&self, context: &mut ManagerContext, _message: &Message) -> EventResult {
-        // TODO: implements.
-        trace!("{}: on_client_done\n", LOG_TAG);
-        Ok(vec![])
+    fn on_client_done(&self, context: &mut ManagerContext, message: &Message) -> EventResult {
+        warn!("{}: on_client_done id={} (nop)", LOG_TAG, message.client_id);
+        let mut responses: Vec<Message> = Vec::new();
+        // update client state.
+        if let Some(client) = context.clients.get_mut(&message.client_id) {
+            match client.state {
+                ClientState::Running { .. } => {
+                    info!(
+                        "{}: send error to client {} done",
+                        LOG_TAG, message.client_id
+                    );
+                    client.set_client_state(ClientState::Exitting);
+                    responses
+                        .push(Message::new(MessageType::Error, message.client_id, None).unwrap());
+                }
+                _ => {
+                    warn!(
+                        "{}: client {} is not in Running, dropped.",
+                        LOG_TAG, message.client_id
+                    );
+                }
+            }
+        }
+        Ok(responses)
     }
 
-    fn on_client_exit(&self, context: &mut ManagerContext, _message: &Message) -> EventResult {
-        // TODO: implements.
-        trace!("{}: on_client_exit\n", LOG_TAG);
-        Ok(vec![])
+    fn on_client_exit(&self, context: &mut ManagerContext, message: &Message) -> EventResult {
+        trace!("{}: on_client_exit id={}", LOG_TAG, message.client_id);
+        let mut responses: Vec<Message> = Vec::new();
+        // update client state.
+        if let Some(client) = context.clients.get_mut(&message.client_id) {
+            if client.state == ClientState::Exitting {
+                info!("{}: client {} is exit", LOG_TAG, message.client_id);
+                client.set_client_state(ClientState::None);
+                context.num_active_clients -= 1;
+                responses.push(Message::new(MessageType::Ok, message.client_id, None).unwrap());
+            } else {
+                warn!(
+                    "{}: client {} is not in Exiting, dropped.",
+                    LOG_TAG, message.client_id
+                );
+            }
+        }
+        // check if all clients are ready.
+        if context.num_active_clients == 0 {
+            context.set_state(ManagerState::Exitted);
+        }
+        Ok(responses)
     }
 
     fn on_shutdown(&self, context: &mut ManagerContext) -> EventResult {
-        // TODO: implements.
-        trace!("{}: on_shutdown\n", LOG_TAG);
+        trace!("{}: on_shutdown", LOG_TAG);
+        // keep going.
         Ok(vec![])
     }
 }
---
+++ server-rs/src/manager/process_exitting_test.rs
@@ -0,0 +1,217 @@
+#[cfg(test)]
+use super::*;
+use crate::config::*;
+
+fn create_context() -> ManagerContext {
+    let mut ctx = ManagerContext::new(vec![
+        ClientConfig::new(0, TriggerType::Cycle(1), 0).unwrap(),
+        ClientConfig::new(1, TriggerType::Cycle(2), 1).unwrap(),
+        ClientConfig::new(2, TriggerType::Depends { clients: vec![0] }, 0).unwrap(),
+    ]);
+    ctx.state = ManagerState::Exitting;
+    ctx.clients.get_mut(&0).unwrap().state = ClientState::Exitting;
+    ctx.clients.get_mut(&1).unwrap().state = ClientState::Idle;
+    ctx.clients.get_mut(&2).unwrap().state = ClientState::Running { cycle: 1 };
+    ctx.num_active_clients = 3;
+    return ctx;
+}
+
+#[test]
+fn test_enter_state() {
+    // create objects.
+    let mut ctx = create_context();
+    let proc = ManagerProcExitting;
+
+    // setup condition.
+    // do nothing.
+
+    // send event.
+    proc.enter_state(&mut ctx);
+
+    // check result.
+    assert_eq!(ctx.state, ManagerState::Exitting);
+    assert_eq!(ctx.state_changed, false);
+    assert_eq!(ctx.clients.get(&0).unwrap().state, ClientState::Exitting);
+    assert_eq!(ctx.clients.get(&1).unwrap().state, ClientState::Idle);
+    assert_eq!(
+        ctx.clients.get(&2).unwrap().state,
+        ClientState::Running { cycle: 1 }
+    );
+    assert_eq!(ctx.num_active_clients, 3);
+}
+
+#[test]
+fn test_on_cycle_start() {
+    // create objects.
+    let mut ctx = create_context();
+    let proc = ManagerProcExitting;
+
+    // setup condition.
+    // do nothing.
+
+    // send event.
+    let result = proc.on_cycle_start(&mut ctx, 100).unwrap();
+
+    // check result.
+    assert_eq!(result.len(), 0);
+    assert_eq!(ctx.state, ManagerState::Exitting);
+    assert_eq!(ctx.state_changed, false);
+    assert_eq!(ctx.clients.get(&0).unwrap().state, ClientState::Exitting);
+    assert_eq!(ctx.clients.get(&1).unwrap().state, ClientState::Idle);
+    assert_eq!(
+        ctx.clients.get(&2).unwrap().state,
+        ClientState::Running { cycle: 1 }
+    );
+    assert_eq!(ctx.num_active_clients, 3);
+}
+
+#[test]
+fn test_on_client_join() {
+    // create objects.
+    let mut ctx = create_context();
+    let proc = ManagerProcExitting;
+
+    // setup condition.
+    // do nothing.
+
+    // send event.
+    let m = Message::new(MessageType::Join, 0, None).unwrap();
+    let result = proc.on_client_join(&mut ctx, &m);
+
+    // check result.
+    assert_eq!(result.is_err(), true);
+}
+
+#[test]
+fn test_on_client_ready() {
+    // create objects.
+    let mut ctx = create_context();
+    let proc = ManagerProcExitting;
+
+    // setup condition.
+    // do nothing.
+
+    // send event.
+    let m = Message::new(MessageType::Ready, 1, None).unwrap();
+    let result = proc.on_client_ready(&mut ctx, &m).unwrap();
+
+    // check result.
+    assert_eq!(result.len(), 1);
+    assert_eq!(result[0].message_type, MessageType::Error);
+    assert_eq!(result[0].client_id, 1);
+    assert_eq!(ctx.state, ManagerState::Exitting);
+    assert_eq!(ctx.state_changed, false);
+    assert_eq!(ctx.clients.get(&0).unwrap().state, ClientState::Exitting);
+    assert_eq!(ctx.clients.get(&1).unwrap().state, ClientState::Exitting);
+    assert_eq!(
+        ctx.clients.get(&2).unwrap().state,
+        ClientState::Running { cycle: 1 }
+    );
+    assert_eq!(ctx.num_active_clients, 3);
+}
+
+#[test]
+fn test_on_client_done() {
+    // create objects.
+    let mut ctx = create_context();
+    let proc = ManagerProcExitting;
+
+    // setup condition.
+    // do nothing.
+
+    // send event.
+    let m = Message::new(MessageType::Done, 2, None).unwrap();
+    let result = proc.on_client_done(&mut ctx, &m).unwrap();
+
+    // check result.
+    assert_eq!(result.len(), 1);
+    assert_eq!(result[0].message_type, MessageType::Error);
+    assert_eq!(result[0].client_id, 2);
+    assert_eq!(ctx.state, ManagerState::Exitting);
+    assert_eq!(ctx.state_changed, false);
+    assert_eq!(ctx.clients.get(&0).unwrap().state, ClientState::Exitting);
+    assert_eq!(ctx.clients.get(&1).unwrap().state, ClientState::Idle);
+    assert_eq!(ctx.clients.get(&2).unwrap().state, ClientState::Exitting);
+    assert_eq!(ctx.num_active_clients, 3);
+}
+
+#[test]
+fn test_on_client_exit() {
+    // create objects.
+    let mut ctx = create_context();
+    let proc = ManagerProcExitting;
+
+    // setup condition.
+    // do nothing.
+
+    // send event.
+    let m = Message::new(MessageType::Exit, 0, None).unwrap();
+    let result = proc.on_client_exit(&mut ctx, &m).unwrap();
+
+    // check result.
+    assert_eq!(result.len(), 1);
+    assert_eq!(result[0].message_type, MessageType::Ok);
+    assert_eq!(result[0].client_id, 0);
+    assert_eq!(ctx.state, ManagerState::Exitting);
+    assert_eq!(ctx.state_changed, false);
+    assert_eq!(ctx.clients.get(&0).unwrap().state, ClientState::None);
+    assert_eq!(ctx.clients.get(&1).unwrap().state, ClientState::Idle);
+    assert_eq!(
+        ctx.clients.get(&2).unwrap().state,
+        ClientState::Running { cycle: 1 }
+    );
+    assert_eq!(ctx.num_active_clients, 2);
+}
+
+#[test]
+fn test_on_client_exit_all() {
+    // create objects.
+    let mut ctx = create_context();
+    let proc = ManagerProcExitting;
+
+    // setup condition.
+    ctx.clients.get_mut(&0).unwrap().state = ClientState::Exitting;
+    ctx.clients.get_mut(&1).unwrap().state = ClientState::None;
+    ctx.clients.get_mut(&2).unwrap().state = ClientState::None;
+    ctx.num_active_clients = 1;
+
+    // send event.
+    let m = Message::new(MessageType::Exit, 0, None).unwrap();
+    let result = proc.on_client_exit(&mut ctx, &m).unwrap();
+
+    // check result.
+    assert_eq!(result.len(), 1);
+    assert_eq!(result[0].message_type, MessageType::Ok);
+    assert_eq!(result[0].client_id, 0);
+    assert_eq!(ctx.state, ManagerState::Exitted);
+    assert_eq!(ctx.state_changed, true);
+    assert_eq!(ctx.clients.get(&0).unwrap().state, ClientState::None);
+    assert_eq!(ctx.clients.get(&1).unwrap().state, ClientState::None);
+    assert_eq!(ctx.clients.get(&2).unwrap().state, ClientState::None);
+    assert_eq!(ctx.num_active_clients, 0);
+}
+
+#[test]
+fn test_on_shutdown() {
+    // create objects.
+    let mut ctx = create_context();
+    let proc = ManagerProcExitting;
+
+    // setup condition.
+    // do nothing.
+
+    // send event.
+    let result = proc.on_shutdown(&mut ctx).unwrap();
+
+    // check result.
+    assert_eq!(result.len(), 0);
+    assert_eq!(ctx.state, ManagerState::Exitting);
+    assert_eq!(ctx.state_changed, false);
+    assert_eq!(ctx.clients.get(&0).unwrap().state, ClientState::Exitting);
+    assert_eq!(ctx.clients.get(&1).unwrap().state, ClientState::Idle);
+    assert_eq!(
+        ctx.clients.get(&2).unwrap().state,
+        ClientState::Running { cycle: 1 }
+    );
+    assert_eq!(ctx.num_active_clients, 3);
+}
